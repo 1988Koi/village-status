3d317fa0c5b22a41527dabf11180ec83a1747aed
Rafael <2404827@escolas.anchieta.br>
2026-09-17 18:43:23 -0300

    Started tinkering with Prometheus (I have no clue what I am doing, everything will be slowed because of that.

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from Main import VillageState, get_uptime_and_build_village, village_from_uptime
 from dataclasses import dataclass, asdict
-
-
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import requests
 
 app = Flask(__name__)
 
+request_counter = Counter("app_request_total", "Total Requests Recieved")
+
+@app.route("/test-prometheus")
+def testprometheus():
+    response = requests.get("http://prometheus:9090/api/v1/query", params={"query": "up"})
+    data = response.json()
+    return str(data)
+
 @app.route("/village-state/<int:uptime>")
 def village(uptime):
+    request_counter.inc()
     return(asdict(village_from_uptime(uptime)))
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
